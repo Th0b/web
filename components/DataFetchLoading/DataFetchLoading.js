5586f15d6cd16a61cc5c71ca6592efcb987d646d
Th0be <navrat.tobias@gmail.com>
2023-02-08 16:08:11 +0000

    Edit error message

--- components/DataFetchLoading/DataFetchLoading.js
+++ components/DataFetchLoading/DataFetchLoading.js
@@ -3,7 +3,12 @@ import styles from "./DataFetchLoading.module.sass";
 
 export default function DataFetchLoading({ error, loading }) {
   if (error) {
-    return <div>Nastala chyba, zkuste obnovit stránku nebo mě kontaktujte</div>;
+    return (
+      <div className={styles.errorContainer}>
+        <img src="error.svg" alt="Error" className={styles.errorCross} />
+        <div className={styles.error}>Při načítání dat nastala chyba, zkus obnovit stránku.</div>
+      </div>
+    );
   } else if (loading) {
     return (
       <div className={styles.loading}>
--- pages/404.js
+++ pages/404.js
@@ -1,8 +1,11 @@
-//Components
-import Link from "next/link";
+//Layouts
 import Header from "layouts/Header/Header";
 import Footer from "layouts/Footer/Footer";
+//Components
+import Link from "next/link";
 import Block from "components/Block/Block";
+//Styles
+import styles from "styles/404.module.sass";
 
 export default function Home() {
   return (
@@ -11,7 +14,12 @@ export default function Home() {
       <main>
         <Block>
           <h1>Stránka nenalezena</h1>
-          <p>Zkus se vrátit na domovskou stránku <Link href="/">tady</Link></p>
+          <div className={styles.errorContainer}>
+            <img src="error.svg" alt="Error" className={styles.errorCross} />
+            <div className={styles.error}>
+              Zkus se vrátit na <Link href="/">domovskou stránku</Link>.
+            </div>
+          </div>
         </Block>
       </main>
       <Footer />
