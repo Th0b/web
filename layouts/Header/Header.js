91450841585c473ac969f6fcfbb1158489764719
Th0be <navrat.tobias@gmail.com>
2023-02-13 17:37:24 +0000

    Add responsivity of header

--- layouts/Header/Header.js
+++ layouts/Header/Header.js
@@ -1,24 +1,51 @@
+//Hooks
+import { useState } from "react";
+import { useWindowWidth } from "@react-hook/window-size";
 //Components
 import Link from "next/link";
+import Hamburger from "hamburger-react";
 //Styles
 import styles from "./styles/Header.module.sass";
 
 export default function Header() {
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+  const windowWidth = useWindowWidth();
+
   return (
-    <header className={styles.header}>
-      <Link href="/">
-        <img className={styles.logo} src="/favicon.ico" alt="Logo" />
-      </Link>
-      <Link href="/">
-        <span className={styles.name}>Tobiáš Návrat</span>
-      </Link>
-      <nav className={styles.pageNavigation}>
-        <Link href="/">Domů</Link>
-        <Link href="/it">IT</Link>
-        <Link href="/photography">Fotografování</Link>
-        <Link href="/about">O mně</Link>
-        <div className={styles.dot}></div>
-      </nav>
-    </header>
+    <>
+      <header className={styles.header}>
+        <Link href="/">
+          <img className={styles.logo} src="/favicon.ico" alt="Logo" />
+        </Link>
+        <Link href="/">
+          <span className={styles.name}>Tobiáš Návrat</span>
+        </Link>
+        {windowWidth <= 770 && (
+          <div className={styles.hamburgerContainer}>
+            <Hamburger toggled={menuIsOpen} toggle={setMenuIsOpen} />
+          </div>
+        )}
+        <nav
+          data-visible={windowWidth > 770 || menuIsOpen}
+          className={styles.navigation}
+        >
+          <ul>
+            <li>
+              <Link href="/">Domů</Link>
+            </li>
+            <li>
+              <Link href="/it">IT</Link>
+            </li>
+            <li>
+              <Link href="/photography">Fotografování</Link>
+            </li>
+            <li>
+              <Link href="/about">O mně</Link>
+            </li>
+            <div className={styles.dot}></div>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 }
