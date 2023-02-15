d63cc806b08da19b8f8a76f9a1f6489a746de117
Th0be <navrat.tobias@gmail.com>
2023-02-15 10:54:58 +0000

    Google analytics bug fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,16 @@
+//Components
+import { GoogleAnalytics } from "nextjs-google-analytics";
 //Styles
 import "../styles/globals.sass";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <GoogleAnalytics
+        gaMeasurementId="G-LPQ6KHGMG7"
+        strategy="lazyOnload"
+        trackPageViews
+      />
       <Component {...pageProps} />
     </>
   );
--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,5 @@
 //Components
 import { Html, Head, Main, NextScript } from "next/document";
-import { GoogleAnalytics } from "nextjs-google-analytics";
 
 export default function Document() {
   return (
@@ -15,7 +14,6 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <GoogleAnalytics gaMeasurementId="G-LPQ6KHGMG7" strategy="lazyOnload" trackPageViews />
       </body>
     </Html>
   );
