44b8918de3d0cd823303f113228c48ae5114bfba
Th0be <navrat.tobias@gmail.com>
2023-02-15 09:35:29 +0000

    Google analytics bug fix

--- components/PageHead/PageHead.js
+++ components/PageHead/PageHead.js
@@ -1,22 +1,8 @@
 import Head from "next/head";
-import Script from "next/script"
 
 const PageHead = ({ title, description }) => {
   return (
     <Head>
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-LPQ6KHGMG7"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-LPQ6KHGMG7');
-        `}
-      </Script>
       <title>{title}</title>
       <meta name="description" content={description} />
     </Head>
