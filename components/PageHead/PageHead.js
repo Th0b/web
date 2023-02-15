d014e24c65d3053b817a0d09eb2f7b7b0200e94d
Th0be <navrat.tobias@gmail.com>
2023-02-15 09:41:00 +0000

    Google analytics fix

--- components/PageHead/PageHead.js
+++ components/PageHead/PageHead.js
@@ -1,11 +1,28 @@
+//Components
 import Head from "next/head";
+import Script from "next/script"
 
 const PageHead = ({ title, description }) => {
   return (
-    <Head>
-      <title>{title}</title>
-      <meta name="description" content={description} />
-    </Head>
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+      </Head>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-LPQ6KHGMG7"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){window.dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-LPQ6KHGMG7');
+        `}
+      </Script>
+    </>
   );
 };
 
--- pages/_document.js
+++ pages/_document.js
@@ -1,23 +1,9 @@
 //Components
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script"
 
 export default function Document() {
   return (
     <Html>
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-LPQ6KHGMG7"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-LPQ6KHGMG7');
-        `}
-      </Script>
       <Head>
         <meta
           name="description"
