9d5f8d1733f3d7aa49f85522f86e8b9a4916e7d9
Th0be <navrat.tobias@gmail.com>
2023-02-15 10:20:54 +0000

    Google analytics bug fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
+//Components
+import { GoogleAnalytics } from "nextjs-google-analytics";
 //Styles
-import '../styles/globals.sass'
+import "../styles/globals.sass";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GoogleAnalytics gaMeasurementId="G-LPQ6KHGMG7" trackPageViews />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,5 @@
 //Components
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 export default function Document() {
   return (
@@ -15,19 +14,6 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <Script
-          strategy="lazyOnload"
-          src={`https://www.googletagmanager.com/gtag/js?id=G-LPQ6KHGMG7`}
-        />
-        <Script id='google-analytics' strategy="lazyOnload">
-          {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-G-LPQ6KHGMG7');
-        `}
-        </Script>
       </body>
     </Html>
   );
