91bd5e9597882180ddb2b03fc6d7e2e66d06df5f
Th0be <navrat.tobias@gmail.com>
2023-02-15 11:10:06 +0000

    Vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,15 @@
-//Components
+//Site measure
 import { GoogleAnalytics } from "nextjs-google-analytics";
+import { Analytics } from "@vercel/analytics/react";
 //Styles
 import "../styles/globals.sass";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <GoogleAnalytics
-        gaMeasurementId="G-LPQ6KHGMG7"
-        trackPageViews
-      />
+      <GoogleAnalytics gaMeasurementId="G-LPQ6KHGMG7" trackPageViews />
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
