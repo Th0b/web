17d667f17616320bd66b8d7b6695be1194ab5bda
Th0be <navrat.tobias@gmail.com>
2023-02-15 10:24:30 +0000

    Google analytics bug fix

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ import "../styles/globals.sass";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <GoogleAnalytics gaMeasurementId="G-LPQ6KHGMG7" trackPageViews />
+      <GoogleAnalytics gaMeasurementId="G-LPQ6KHGMG7" strategy="lazyOnload" trackPageViews />
       <Component {...pageProps} />
     </>
   );
