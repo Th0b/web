dfc91f8a854f5728e588b9ca6444ed25c785efc2
Th0be <th0betheth0be@gmail.com>
2024-03-26 00:18:41 +0100

    Gallery fixed

--- layouts/Photos/Photos.js
+++ layouts/Photos/Photos.js
@@ -3,11 +3,12 @@ import React, { useEffect, useState } from "react";
 //Utils
 import { dataFetch } from "utils/dataFetch";
 import FetchProcess from "components/FetchProcess/FetchProcess.js";
-import "photoswipe/dist/photoswipe.css";
 import { Gallery, Item } from "react-photoswipe-gallery";
 //Constants
 import Constants from "/constants";
 //Styles
+import "photoswipe/dist/photoswipe.css";
+import styles from "./styles/Photos.module.sass";
 
 export default function Photos({ quantity }) {
   const [photos, setPhotos] = useState();
@@ -23,26 +24,49 @@ export default function Photos({ quantity }) {
       <FetchProcess status={status} />
       {status === "success" && photos !== undefined && (
         <Gallery>
-          {photos.result.images.map((image, index) => (
-            <>
-              <Item
-                original={image.element_url}
-                thumbnail={image.derivatives.thumb.url}
-                width={image.width}
-                height={image.height}
-              >
-                {({ ref, open }) => (
-                  <img
-                    ref={ref}
-                    onClick={open}
-                    src={image.derivatives.thumb.url}
-                  />
-                )}
-              </Item>
-            </>
-          ))}
+          <div className={styles.wrapper}>
+            {photos.result.images.map((image, index) => (
+              <Photo image={image} key={index} />
+            ))}
+          </div>
         </Gallery>
       )}
     </>
   );
+}
+
+function Photo({ image }) {
+  const style = {
+    gridColumnEnd: `span ${getSpanEstimate(image.width)}`,
+    gridRowEnd: `span ${getSpanEstimate(image.height)}`,
+  };
+
+  return (
+    <>
+      <Item
+        original={image.element_url}
+        thumbnail={image.derivatives.small.url}
+        width={image.width}
+        height={image.height}
+      >
+        {({ ref, open }) => (
+          <img
+            className={styles.photo}
+            style={style}
+            ref={ref}
+            onClick={open}
+            src={image.derivatives.small.url}
+          />
+        )}
+      </Item>
+    </>
+  );
+}
+
+function getSpanEstimate(size) {
+  if (size > 3000) {
+    return 2;
+  }
+
+  return 1;
 }
