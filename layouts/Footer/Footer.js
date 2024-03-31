68f68f18e4d80ed762c78dab4af3e4ed8da19a50
Th0be <th0betheth0be@gmail.com>
2024-03-31 20:23:17 +0200

    Vercel tracking

--- layouts/Footer/Footer.js
+++ layouts/Footer/Footer.js
@@ -2,6 +2,8 @@
 import Constants from "/constants";
 //Styles
 import styles from "./styles/Footer.module.sass";
+// Vercel
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function Footer() {
   const yearNow = new Date().getFullYear();
@@ -12,62 +14,73 @@ export default function Footer() {
   const linkedinIcon = "/SocialSites/linkedin.png";
 
   return (
-    <footer className={styles.footer}>
-      <div>
-        <a
-          href={Constants.GITHUB_ISSUE}
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Nahlásit chybu
-        </a>
-      </div>
-      |<div>&#169; 2022-{yearNow}</div>|<div>Tobiáš Návrat</div>|
-      <div>
-        <a
-          href={Constants.BUYMEACOFFEE_URL}
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          <img
-            className={styles.icon}
-            src={buymeacoffeeIcon}
-            alt="Buymeacoffee Logo"
-          />
-        </a>
-        <a
-          href={Constants.FACEBOOK_URL}
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          <img className={styles.icon} src={facebookIcon} alt="Facebook Logo" />
-        </a>
-        <a
-          href={Constants.INSTAGRAM_URL}
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          <img
-            className={styles.icon}
-            src={instagramIcon}
-            alt="Instagram Logo"
-          />
-        </a>
-        <a
-          href={Constants.GITHUB_URL}
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          <img className={styles.icon} src={githubIcon} alt="Github Logo" />
-        </a>
-        <a
-          href={Constants.LINKEDIN_URL}
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          <img className={styles.icon} src={linkedinIcon} alt="Linkedin Logo" />
-        </a>
-      </div>
-    </footer>
+    <>
+      <footer className={styles.footer}>
+        <div>
+          <a
+            href={Constants.GITHUB_ISSUE}
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Nahlásit chybu
+          </a>
+        </div>
+        |<div>&#169; 2022-{yearNow}</div>|<div>Tobiáš Návrat</div>|
+        <div>
+          <a
+            href={Constants.BUYMEACOFFEE_URL}
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <img
+              className={styles.icon}
+              src={buymeacoffeeIcon}
+              alt="Buymeacoffee Logo"
+            />
+          </a>
+          <a
+            href={Constants.FACEBOOK_URL}
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <img
+              className={styles.icon}
+              src={facebookIcon}
+              alt="Facebook Logo"
+            />
+          </a>
+          <a
+            href={Constants.INSTAGRAM_URL}
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <img
+              className={styles.icon}
+              src={instagramIcon}
+              alt="Instagram Logo"
+            />
+          </a>
+          <a
+            href={Constants.GITHUB_URL}
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <img className={styles.icon} src={githubIcon} alt="Github Logo" />
+          </a>
+          <a
+            href={Constants.LINKEDIN_URL}
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <img
+              className={styles.icon}
+              src={linkedinIcon}
+              alt="Linkedin Logo"
+            />
+          </a>
+        </div>
+      </footer>
+      <SpeedInsights />
+    </>
   );
 }
